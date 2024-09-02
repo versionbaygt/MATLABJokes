e109dab70657d1ef11eef55f7ce713f0046f4c91
Gareth Thomas <gareth@versionbay.com>
2024-09-02 07:37:47 +0200

    fix: update buildfile for github release and bump release

--- buildfile.m
+++ buildfile.m
@@ -21,7 +21,35 @@ end
 
 function releaseTask(~)
 projectFile = "toolboxPackaging.prj";
+currentPath = pwd; % Needed of creating releases
 opts = matlab.addons.toolbox.ToolboxOptions(projectFile);
-opts.OutputFile = opts.OutputFile.replace(" in MATLAB","")
+opts.OutputFile = opts.OutputFile.replace(" in MATLAB","");
+opts.ToolboxGettingStartedGuide = fullfile(currentPath,"toolbox","doc","GettingStarted.mlx");
+opts.ToolboxImageFile = fullfile(currentPath,"images","Jokes in MATLAB.png");
+opts.ToolboxFiles = [
+    fullfile(currentPath,"toolbox","app","displayJoke.html");
+    fullfile(currentPath,"toolbox","app","jokeApp.mlapp");
+    fullfile(currentPath,"toolbox","app","jokeApp.mlappinstall");
+    fullfile(currentPath,"toolbox","app","jokeApp_resources","icon_16.png");
+    fullfile(currentPath,"toolbox","app","jokeApp_resources","icon_24.png");
+    fullfile(currentPath,"toolbox","app","jokeApp_resources","icon_48.png");
+    fullfile(currentPath,"toolbox","doc","GettingStarted.mlx");
+    fullfile(currentPath,"toolbox","doc","helptoc.xml");
+    fullfile(currentPath,"toolbox","info.xml");
+    fullfile(currentPath,"toolbox","internal","get_joke.m");
+    fullfile(currentPath,"toolbox","joke.m");
+    fullfile(currentPath,"toolbox","tests","test_get_joke.m");
+    fullfile(currentPath,"toolbox","tests","test_joke.m");
+    fullfile(currentPath,"toolbox","tests","test_jokeApp.m");
+    ];
+opts.ToolboxMatlabPath = [fullfile(currentPath,"toolbox");
+    fullfile(currentPath,"toolbox","app");
+    fullfile(currentPath,"toolbox","app","jokeApp_resources");
+    fullfile(currentPath,"toolbox","doc");
+    fullfile(currentPath,"toolbox","internal");
+    fullfile(currentPath,"toolbox","tests");
+    ];
+
+opts.ToolboxVersion = "0.3.3";                           
 matlab.addons.toolbox.packageToolbox(opts);
 end
